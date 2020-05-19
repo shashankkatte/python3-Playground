e7d10a0d70039b1f77a60a579d917d89af0bca8a
shashankkatte <shashankanand.katte@gmail.com>
2020-05-19 11:01:43 -0400

    List manipulation demo

--- 01_Basics/basics.py
+++ 01_Basics/basics.py
@@ -1,2 +1,9 @@
 import datetime
-print("The date time is", datetime.datetime.now())
+mynow = datetime.datetime.now()
+print("The date time is", mynow)
+
+student_grades = [9.1, 8.8, 7.5]
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print("Mean of student grades", mean)
